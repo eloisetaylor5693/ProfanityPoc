c653018aa0ef4500dacad13c4d12320814686a78
Eloise Taylor <eloise.taylor@reedonline.co.uk>
2019-11-29 15:15:38 +0000

    Tested out ProfanityDetector

---
+++ ProfanityPoc/ProfanityDetectorTests.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+
+namespace ProfanityPoc
+{
+    public class ProfanityDetectorTests
+    {
+        [TestCase("hello")]
+        [TestCase("Scunthorpe")]
+        [TestCase("flowers")]
+        public void GivenWordsWhichAreNotProfane_IsProfanity_IsFalse(string wordWhichIsNotProfane)
+        {
+            var filter = new ProfanityFilter.ProfanityFilter();
+            var result = filter.IsProfanity("wordWhichIsNotProfane");
+            Assert.That(result, Is.False);
+        }
+
+        [TestCase("shitface wonderer")]
+        [TestCase("SHITFACE")]
+        public void GivenProfanities_IsProfanity_IsTrue(string profanity)
+        {
+            var filter = new ProfanityFilter.ProfanityFilter();
+            var result = filter.IsProfanity(profanity);
+            Assert.That(result, Is.True);
+        }
+        
+        [Test]
+        public void GivenProfanityThatLibraryDoesNotRecognise_IsProfanity_IsFalse()
+        {
+            var filter = new ProfanityFilter.ProfanityFilter();
+            var result = filter.IsProfanity("sh1tface");
+            Assert.That(result, Is.True);
+        }
+    }
+}
