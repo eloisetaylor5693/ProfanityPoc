23186e37aaa34bbac1bfe5423c388448a8141876
Eloise Taylor <rockspud5693@gmail.com>
2019-12-01 15:41:53 +0000

    Testing CensorString()

---
+++ ProfanityPoc/ProfanityDetectorLib/CensorStringTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace ProfanityPoc.ProfanityDetectorLib
+{
+    [TestFixture]
+    public sealed class CensorStringTests
+    {
+        [Test]
+        public void GivenSentenceWithProfanity_DetectAllProfanities_FlagsProfanities()
+        {
+            var filter = new ProfanityFilter.ProfanityFilter();
+            var result = filter.CensorString("0 shits given");
+
+            Assert.That(result, Is.EqualTo("0 ***** given"));
+        }
+    }
+}
