2a95fabf5013dd0659dd18afc11e13173c499e8a
Eloise Taylor <rockspud5693@gmail.com>
2019-12-01 15:41:22 +0000

    Testing DetectAllProfanities()

---
+++ ProfanityPoc/ProfanityDetectorLib/DetectAllProfanitiesTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace ProfanityPoc.ProfanityDetectorLib
+{
+    [TestFixture]
+    public sealed class DetectAllProfanitiesTests
+    {
+        [Test]
+        public void GivenSentenceWithProfanity_DetectAllProfanities_FlagsProfanities()
+        {
+            var filter = new ProfanityFilter.ProfanityFilter();
+            var result = filter.DetectAllProfanities("0 shits given");
+
+            Assert.That(result, Contains.Item("shits"));
+        }
+    }
+}
